368fccfbce2c7a7f9dc6c95eb2dabdb7a9f54c7b
Islam Mahmoud <islam.mahmoud@booking.com>
2019-10-03 14:28:23 +0200

    update 1.1

--- run_c_tags.sh
+++ run_c_tags.sh
@@ -1,7 +1,6 @@
-DIR="$HOME/work"
+DIR="$HOME/git_tree"
 echo "Start generating ctags"
 cd $DIR
 rm -rf $DIR/tags
-/usr/local/bin/ctags --languages=perl  --exclude=*.pl --exclude=*.log -f $DIR/tags.new -R $DIR/main/lib
-/usr/local/bin/ctags --languages=perl  --exclude=*.pl --exclude=*.log -f $DIR/tags.new -R $DIR/main/apps/bookings2
+/usr/local/bin/ctags --languages=perl  --extra=+q --exclude=*.pl --exclude=*.log -f $DIR/tags.new -R  $DIR/main/lib $DIR/main/webservice/bpayprocessor
 mv $DIR/tags.new $DIR/tags
